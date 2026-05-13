606e33c3e3412ff523ec04d00d336ee14d660ff6
spahicadis <adis@spahica.com>
2026-05-13 22:22:10 +0200

    feat: implemented assets for objects in Objectives scene, validation of opening vault in different cases, exported objects into prefabs

--- Assets/Scripts/Objectives/VaultInteraction.cs
+++ Assets/Scripts/Objectives/VaultInteraction.cs
@@ -1,31 +1,51 @@
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
 public class VaultInteraction : MonoBehaviour
 {
-    [Header("Vault Settings")]
-    public Material completedMaterial;
-    
-    [Header("UI")]
+    public Transform vaultDoor;
     public GameObject missionCompleteUI;
+    public GameObject keycardWarningUI;
+
+    private bool isOpening = false;
+    private Quaternion targetRotation = Quaternion.Euler(-90f, -100f, 0f);
+
+    private void Update()
+    {
+        if (isOpening && vaultDoor != null)
+        {
+            vaultDoor.localRotation = Quaternion.RotateTowards(
+                vaultDoor.localRotation,
+                targetRotation,
+                30f * Time.deltaTime
+            );
+
+            if (vaultDoor.localRotation == targetRotation)
+                isOpening = false;
+        }
+    }
 
     private void OnMouseDown()
     {
         if (GameState.Instance.hasKeycard)
         {
             GameState.Instance.CompleteMission();
-            
-            
-            GetComponent<Renderer>().material = completedMaterial;
-            
-            
+            isOpening = true;
             if (missionCompleteUI != null)
                 missionCompleteUI.SetActive(true);
         }
         else
         {
-            Debug.Log("Potrebna keyecard prvo");
+            if (keycardWarningUI != null)
+            {
+                keycardWarningUI.SetActive(true);
+                Invoke(nameof(HideWarning), 2f);
+            }
         }
+    }
+
+    private void HideWarning()
+    {
+        if (keycardWarningUI != null)
+            keycardWarningUI.SetActive(false);
     }
 }
